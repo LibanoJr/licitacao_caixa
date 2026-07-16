ffb0b3ea8289dc019e746c5b4eef6eec2ccfe048
LibanoJr <104811313+LibanoJr@users.noreply.github.com>
2026-07-16 09:54:26 -0300

    Update extract.js

--- api/extract.js
+++ api/extract.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
 
   const { fileBase64, SYSTEM_PROMPT } = req.body;
   const apiKey = process.env.GEMINI_API_KEY;
-  const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-001:generateContent?key=${apiKey}`;
+  const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${apiKey}`;
   
   try {
     const response = await fetch(url, {
